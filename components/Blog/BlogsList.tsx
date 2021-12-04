d0896fd04e80552716f10813c1b0552b69240b8e
haisontop <haisontop@gmail.com>
2021-12-04 12:22:09 -0800

    add mock blogs consts

--- components/Blog/BlogsList.tsx
+++ components/Blog/BlogsList.tsx
@@ -1,29 +1,20 @@
 import React from "react";
+import { MOCK_BLOGS } from "../../constants/mockBlogs";
 import BlogCard from "./BlogCard";
 
 const BlogsList = () => {
-
   return (
     <div>
       <div className="grid grid-cols-2 gap-10 mt-10">
-        <BlogCard
-          title="The feature of CX"
-          createdAt="November 2021"
-          description="Tecnology should work for you, not the other way round. we can help
-          you a powerful software ecosystem that gives your customers
-          amazing experiences and put world-class capabilities in the
-          hands of your team."
-          imagePath="https://thumbs.dreamstime.com/b/men-hair-beauty-handsome-man-model-black-hair-beard-men-hair-beauty-handsome-man-model-black-hair-beard-blue-125031222.jpg"
-        />
-        <BlogCard
-          title="So, What is the metaverse"
-          createdAt="November 2021"
-          description="Tecnology should work for you, not the other way round. we can help
-          you a powerful software ecosystem that gives your customers
-          amazing experiences and put world-class capabilities in the
-          hands of your team."
-          imagePath="https://ichef.bbci.co.uk/news/976/cpsprodpb/179AB/production/_121538669_woman2.jpg"
-        />
+        {MOCK_BLOGS.map((blog) => (
+          <BlogCard
+            key={blog.slug}
+            title={blog.title}
+            createdAt={blog.createdAt}
+            description={blog.description}
+            imagePath={blog.imagePath}
+          />
+        ))}
         <BlogCard
           title="Move it, or lose it."
           createdAt="November 2021"
@@ -60,7 +51,6 @@ const BlogsList = () => {
           hands of your team."
           imagePath="https://thumbs.dreamstime.com/b/men-hair-beauty-handsome-man-model-black-hair-beard-men-hair-beauty-handsome-man-model-black-hair-beard-blue-125031222.jpg"
         />
-
       </div>
       <div className="flex justify-center mt-32">
         <p className="mx-5 text-2xl">1</p>
---
+++ constants/mockBlogs.ts
@@ -0,0 +1,19 @@
+export const MOCK_BLOGS = [
+  {
+    slug: "feature-of-cx",
+    title: "The feature of CX",
+    createdAt: "November 2021",
+    description:
+      "Tecnology should work for you, not the other way round. we can help you a powerful software ecosystem that gives your customers amazing experiences and put world-class capabilities in the hands of your team.",
+    imagePath:
+      "https://thumbs.dreamstime.com/b/men-hair-beauty-handsome-man-model-black-hair-beard-men-hair-beauty-handsome-man-model-black-hair-beard-blue-125031222.jpg",
+  },
+  {
+    title: "So, What is the metaverse",
+    createdAt: "November 2021",
+    description:
+      "Tecnology should work for you, not the other way round. we can help you a powerful software ecosystem that gives your customers amazing experiences and put world-class capabilities in the hands of your team.",
+    imagePath:
+      "https://ichef.bbci.co.uk/news/976/cpsprodpb/179AB/production/_121538669_woman2.jpg",
+  },
+];
