9cb56f4eb6d599fbe3fe6de04885b4dfe7ed05a9
MridulSheikh <prince9nazir@gmail.com>
2021-12-09 19:38:19 +0600

    new update home page

--- components/Services/ServiceBody.tsx
+++ components/Services/ServiceBody.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const ServiceBody = () => {
     return (
-        <div className="text-center">
-            <h1 className="md:text-5xl mt-32 mb-36">
-            WE help e-commerce brands <br /> strike the right balance between <br /> high-touch marketing and <br /> protecting brand reputation to <br /> delevery  memorable customer <br /> experiences and grow revenue.
+        <div className="text-center px-4 md:px-0">
+            <h1 className="md:text-5xl text-3xl mt-32 mb-36">
+                WE help e-commerce brands <br /> strike the right balance between <br /> high-touch marketing and <br /> protecting brand reputation to <br /> delevery  memorable customer <br /> experiences and grow revenue.
             </h1>
         </div>
     );
--- components/Services/ServiceSpaciality.tsx
+++ components/Services/ServiceSpaciality.tsx
@@ -2,23 +2,23 @@ import React from 'react';
 
 const ServiceSpaciality = () => {
     return (
-        <div className="text-center mt-40">
+        <div className="text-center mt-40 px-5 pb-14 md:pb-0">
             <div>
-            <h1 className="text-white text-5xl">Our spacialties</h1>
+                <h1 className="text-white text-5xl">Our spacialties</h1>
             </div>
-            <div className="grid grid-cols-4 gap-5 mt-20">
-            <div className="bg-white rounded-full flex justify-center">
-            <img src="https://i0.wp.com/akersdigital.com/wp-content/uploads/2019/06/Klaviyo-Partner.png?resize=600%2C200&ssl=1" width="90%" alt="" />
-            </div>
-            <div className="bg-white rounded-full flex justify-center">
-            <img src="https://i0.wp.com/akersdigital.com/wp-content/uploads/2019/06/Klaviyo-Partner.png?resize=600%2C200&ssl=1" width="90%" height="100%" alt="" />
-            </div>
-            <div className="bg-white rounded-full flex justify-center">
-            <img src="https://i0.wp.com/akersdigital.com/wp-content/uploads/2019/06/Klaviyo-Partner.png?resize=600%2C200&ssl=1" width="90%" alt="" />
-            </div>
-            <div className="bg-white rounded-full flex justify-center">
-            <img src="https://i0.wp.com/akersdigital.com/wp-content/uploads/2019/06/Klaviyo-Partner.png?resize=600%2C200&ssl=1" width="90%" alt="" />
-            </div>
+            <div className="grid grid-cols-2 md:grid-cols-4 gap-5 mt-20">
+                <div className="bg-white rounded-full flex justify-center">
+                    <img src="https://i0.wp.com/akersdigital.com/wp-content/uploads/2019/06/Klaviyo-Partner.png?resize=600%2C200&ssl=1" width="90%" alt="" />
+                </div>
+                <div className="bg-white rounded-full flex justify-center">
+                    <img src="https://i0.wp.com/akersdigital.com/wp-content/uploads/2019/06/Klaviyo-Partner.png?resize=600%2C200&ssl=1" width="90%" height="100%" alt="" />
+                </div>
+                <div className="bg-white rounded-full flex justify-center">
+                    <img src="https://i0.wp.com/akersdigital.com/wp-content/uploads/2019/06/Klaviyo-Partner.png?resize=600%2C200&ssl=1" width="90%" alt="" />
+                </div>
+                <div className="bg-white rounded-full flex justify-center">
+                    <img src="https://i0.wp.com/akersdigital.com/wp-content/uploads/2019/06/Klaviyo-Partner.png?resize=600%2C200&ssl=1" width="90%" alt="" />
+                </div>
             </div>
         </div>
     );
