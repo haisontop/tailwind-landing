2db65df83497151814cab564a9d6ca06875ea192
MridulSheikh <prince9nazir@gmail.com>
2021-12-09 23:34:49 +0600

    home changes

--- components/Home/Blogs.tsx
+++ components/Home/Blogs.tsx
@@ -34,13 +34,13 @@ const Blogs = ({ blogs }) => {
             <h1 className="md:text-6xl text-4xl">The Write Up</h1>
             <p className="md:my-10 md-5"></p>
             <Link href="/blogs">
-              <a className="bg-yellow-600 text-black py-3 md:py-5 my-10 md:my-0 rounded-full text-3xl px-10">
+              <a className="bg-yellow-600 text-black py-3 md:py-5 my-10 md:my-0 rounded-full text-3xl px-10 relative top-7">
                 READ ALL
               </a>
             </Link>
           </div>
         </div>
-        <div className="grid grid-cols-1 gap-16">
+        <div className="grid grid-cols-1 gap-16 mt-32 md:mt-0">
           {blogs.map((blog) => (
             <div className="">
               <HomeBlog
