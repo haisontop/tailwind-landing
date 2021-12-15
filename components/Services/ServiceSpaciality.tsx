e7b36ed88e507240eae3b1d2c52d122a28fd4a68
MridulSheikh <prince9nazir@gmail.com>
2021-12-15 22:27:50 +0600

    add images

--- components/Services/ServiceSpaciality.tsx
+++ components/Services/ServiceSpaciality.tsx
@@ -10,14 +10,14 @@ const ServiceSpaciality = () => {
                 <div className="bg-white rounded-full flex justify-center">
                     <img src="https://i0.wp.com/akersdigital.com/wp-content/uploads/2019/06/Klaviyo-Partner.png?resize=600%2C200&ssl=1" width="90%" alt="" />
                 </div>
-                <div className="bg-white rounded-full flex justify-center">
-                    <img src="https://i0.wp.com/akersdigital.com/wp-content/uploads/2019/06/Klaviyo-Partner.png?resize=600%2C200&ssl=1" width="90%" height="100%" alt="" />
+                <div className="bg-white rounded-full">
+                    <img src="/images/shopify.png" width="80%" className='mx-auto my-5' />
                 </div>
                 <div className="bg-white rounded-full flex justify-center">
-                    <img src="https://i0.wp.com/akersdigital.com/wp-content/uploads/2019/06/Klaviyo-Partner.png?resize=600%2C200&ssl=1" width="90%" alt="" />
+                    <img src="/images/solutionPartner.png" width="70%" className='mx-auto my-4' />
                 </div>
-                <div className="bg-white rounded-full flex justify-center">
-                    <img src="images/magentos.png" width="90%" alt="" />
+                <div className="bg-white rounded-full">
+                    <img src="/images/magentos.png" width="80%" className='mx-auto my-4' alt="" />
                 </div>
             </div>
         </div>
